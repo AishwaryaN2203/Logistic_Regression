560814164568ffcf7fac018ff8beb6bbe133130f
AishwaryaNagaraj <aish623n@gmail.com>
2023-02-14 18:28:16 -0600

    feat:Add Train model

---
+++ Logistic_regression_scratch/train.py
@@ -0,0 +1,28 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+
+from Logistic_Regression import LogisticRegression
+
+bc = datasets.load_breast_cancer()
+X, y = bc.data, bc.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+clf = LogisticRegression(0.1)
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+
+def accuracy(y_pred, y_test):
+    return np.sum(y_pred==y_test)/len(y_test)
+
+
+acc = accuracy(y_pred, y_test)
+print(acc)
+
+
+# without passing the learning rate (current rate = 0.001)
+# 0.8947
+
+# increase the learning rate to 0.1 --> accuracy is increase to 0.8947
